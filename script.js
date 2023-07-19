ec611638a8a104421eefc1d1fd6fd72d46a2d67b
mouseanontest <92547327+mouseanontest@users.noreply.github.com>
2023-07-19 17:02:30 -0500

    script.js
    update to scriptjs

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+//This code will make it so that it will load 5 customers in and when you click anywhere on the screen, a new random customer will pop up from one of the 5 loaded customers.
+//I haven't made the customer images yet, so it's gonna look weird when you run it, but we're doing art tomorrow, so I'll add it then.
+
 const config = {
     type: Phaser.AUTO,
     width: '100%',
     height: '100%',
-    parent: 'game',
-
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-
     scene: {
         preload: preload,
         create: create,
@@ -18,9 +18,21 @@ const config = {
 
 const game = new Phaser.Game(config);
 
+const customer1_size = 50;
+const customer2_size = 75;
+const customer3_size = 100;
+const customer4_size = 125;
+const customer5_size = 150;
+
+let clickEnabled = true;
+
 function preload() {
-    this.load.image('background', 'images/background.png');
-    // Load any other assets you might need (e.g., lemonade stand, customers, etc.)
+    this.load.image('background', 'background.png');
+    this.load.image('customer1', 'customer1.png');
+    this.load.image('customer2', 'customer2.png');
+    this.load.image('customer3', 'customer3.png');
+    this.load.image('customer4', 'customer4.png');
+    this.load.image('customer5', 'customer5.png');
 }
 
 function create() {
@@ -28,10 +40,22 @@ function create() {
     backgroundImage.displayWidth = game.config.width;
     backgroundImage.displayHeight = game.config.height;
 
-    // Set up your game elements here, such as the lemonade stand, customers, etc.
+    this.input.on('pointerdown', onPointerDown);
+}
+
+function onPointerDown(pointer) {
+    if (clickEnabled) {
+        const centerX = game.config.width / 2;
+        const centerY = game.config.height / 2;
+        const customerKey = 'customer' + Phaser.Math.Between(1, 5);
+        const customerSize = Phaser.Math.Between(customer1_size, customer5_size);
+        const customer = this.add.image(centerX, centerY, customerKey);
+        const scalePercentage = customerSize / 100;
+        customer.setScale(scalePercentage);
+        clickEnabled = false;
+    }
 }
 
 function update() {
     // Implement your game's logic here
-    // This function runs continuously in your game loop
 }
