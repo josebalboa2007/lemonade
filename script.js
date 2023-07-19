b9630d6bef2d7489c50a1c0d47583a3d403e6549
Justin Lopato <justin@lopato.org>
2023-07-19 15:37:03 -0500

    setup

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const config = {
     type: Phaser.AUTO,
     width: '100%',
     height: '100%',
+    parent: 'game',
+
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
+
     scene: {
         preload: preload,
         create: create,
